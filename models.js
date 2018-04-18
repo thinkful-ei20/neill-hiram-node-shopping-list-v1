eec0faa0b63db90373c485a57051c8ee25da8703
neillsom <neillsom@mail.com>
2018-04-18 08:52:50 -0700

    updated/changed to v2

--- models.js
+++ models.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const uuid = require('uuid');
 
 // this module provides volatile storage, using a `ShoppingList`
@@ -14,86 +12,86 @@ const uuid = require('uuid');
 
 
 function StorageException(message) {
-    this.message = message;
-    this.name = 'StorageException';
+   this.message = message;
+   this.name = "StorageException";
 }
 
 const ShoppingList = {
-    create: function(name, budget) {
-        console.log('Creating new shopping list item');
-        const item = {
-            name: name,
-            id: uuid.v4(),
-            budget: budget
-        };
-        this.items[item.id] = item;
-        return item;
-    },
-    get: function() {
-        console.log('Retrieving shopping list items');
-        return Object.keys(this.items).map(key => this.items[key]);
-    },
-    delete: function(id) {
-        console.log(`Deleting shopping list item \`${id}\``);
-        delete this.items[id];
-    },
-    update: function(updatedItem) {
-        console.log(`Deleting shopping list item \`${updatedItem.id}\``);
-        const {id} = updatedItem;
-        if (!(id in this.items)) {
-            throw StorageException(
-                `Can't update item \`${id}\` because doesn't exist.`);
-        }
-        this.items[updatedItem.id] = updatedItem;
-        return updatedItem;
+  create: function(name, budget) {
+    console.log('Creating new shopping list item');
+    const item = {
+      name: name,
+      id: uuid.v4(),
+      budget: budget
+    };
+    this.items[item.id] = item;
+    return item;
+  },
+  get: function() {
+    console.log('Retrieving shopping list items');
+    return Object.keys(this.items).map(key => this.items[key]);
+  },
+  delete: function(id) {
+    console.log(`Deleting shopping list item \`${id}\``);
+    delete this.items[id];
+  },
+  update: function(updatedItem) {
+    console.log(`Deleting shopping list item \`${updatedItem.id}\``);
+    const {id} = updatedItem;
+    if (!(id in this.items)) {
+      throw StorageException(
+        `Can't update item \`${id}\` because doesn't exist.`)
     }
+    this.items[updatedItem.id] = updatedItem;
+    return updatedItem;
+  }
 };
 
 function createShoppingList() {
-    const storage = Object.create(ShoppingList);
-    storage.items = {};
-    return storage;
+  const storage = Object.create(ShoppingList);
+  storage.items = {};
+  return storage;
 }
 
 const Recipes = {
-    create: function(name, ingredients) {
-        console.log('Creating a new recipe');
-        const item = {
-            name: name,
-            id: uuid.v4(),
-            ingredients: ingredients
-        };
-        this.items[item.id] = item;
-        return item;
-    },
-    get: function() {
-        console.log('Retreiving recipes');
-        return Object.keys(this.items).map(key => this.items[key]);
-    },
-    delete: function(itemId) {
-        console.log(`Deleting recipe with id \`${itemId}\``);
-        delete this.items[itemId];
-    },
-    update: function(updatedItem) {
-        console.log(`Updating recipe with id \`${updatedItem.id}\``);
-        const {id} = updatedItem;
-        if (!(id in this.items)) {
-            throw StorageException(
-                `Can't update item \`${id}\` because doesn't exist.`);
-        }
-        this.items[updatedItem.id] = updatedItem;
-        return updatedItem;
+  create: function(name, ingredients) {
+    console.log('Creating a new recipe');
+    const item = {
+      name: name,
+      id: uuid.v4(),
+      ingredients: ingredients
+    };
+    this.items[item.id] = item;
+    return item;
+  },
+  get: function() {
+    console.log('Retreiving recipes');
+    return Object.keys(this.items).map(key => this.items[key]);
+  },
+  delete: function(itemId) {
+    console.log(`Deleting recipe with id \`${itemId}\``);
+    delete this.items[itemId];
+  },
+  update: function(updatedItem) {
+    console.log(`Updating recipe with id \`${updatedItem.id}\``);
+    const {id} = updatedItem;
+    if (!(id in this.items)) {
+      throw StorageException(
+        `Can't update item \`${id}\` because doesn't exist.`)
     }
+    this.items[updatedItem.id] = updatedItem;
+    return updatedItem;
+  }
 };
 
 
 function createRecipes() {
-    const storage = Object.create(Recipes);
-    storage.items = {};
-    return storage;
+  const storage = Object.create(Recipes);
+  storage.items = {};
+  return storage;
 }
 
 module.exports = {
-    ShoppingList: createShoppingList(),
-    Recipes: createRecipes()
-};
+  ShoppingList: createShoppingList(),
+  Recipes: createRecipes()
+}
--- server.js
+++ server.js
@@ -1,16 +1,10 @@
-'use strict';
 
 const express = require('express');
-// we'll use morgan to log the HTTP layer
+const router = express.Router();
 const morgan = require('morgan');
-// we'll use body-parser's json() method to 
-// parse JSON data sent in requests to this app
 const bodyParser = require('body-parser');
 
-// we import the ShoppingList model, which we'll
-// interact with in our GET endpoint
-const {ShoppingList} = require('./models');
-const {Recipes} = require('./models');
+const {ShoppingList, Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -19,29 +13,45 @@ const app = express();
 app.use(morgan('common'));
 
 // we're going to add some items to ShoppingList
-// so there's some data to look at. Note that 
-// normally you wouldn't do this. Usually your
-// server will simply expose the state of the
-// underlying database.
+// so there's some data to look at
 ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
-Recipes.create('chocolate milk', ['cocoa', 'milk', 'sugar'])
-Recipes.create('chocolate syrup', ['cocoa', 'water', 'sugar'])
-Recipes.create('chocolate cake', ['flour', 'milk', 'sugar'])
+// adding some recipes to `Recipes` so there's something
+// to retrieve.
+Recipes.create(
+  'boiled white rice', ['1 cup white rice', '2 cups water', 'pinch of salt']);
+Recipes.create(
+  'milkshake', ['2 tbsp cocoa', '2 cups vanilla ice cream', '1 cup milk']);
 
-// when the root of this route is called with GET, return
-// all current ShoppingList items by calling `ShoppingList.get()`
+// when the root of this router is called with GET, return
+// all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
-    res.json(ShoppingList.get());
+  res.json(ShoppingList.get());
 });
+
+app.post('/shopping-list', jsonParser, (req, res) => {
+  // ensure `name` and `budget` are in request body
+  const requiredFields = ['name', 'budget'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  const item = ShoppingList.create(req.body.name, req.body.budget);
+  res.status(201).json(item);
+});
+
 
 app.get('/recipes', (req, res) => {
-    res.json(Recipes.get());
-});
-
+  res.json(Recipes.get());
+})
 
 app.listen(process.env.PORT || 8080, () => {
-    console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
